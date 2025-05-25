72df595bf2094591af6ee59034dadb2a67d61e55
thebeni0 <thebeni0@gmail.com>
2025-05-24 19:37:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,14 @@ def generate_image():
         return jsonify({"error": "Prompt required"}), 400
 
     try:
-        response = openai.images.generate(
-            model="dall-e-3",
+        response = openai.Image.create(
             prompt=prompt,
-            size="512x512",
-            n=1
+            n=1,
+            size="512x512"
         )
-        print("RAW OPENAI RESPONSE:", response)  # 👈 Add this line
+        print("RAW OPENAI RESPONSE:", response)
 
-        # Depending on the SDK version, use one of the following:
-        try:
-            image_url = response.data[0].url
-        except AttributeError:
-            image_url = response['data'][0]['url']
-
+        image_url = response["data"][0]["url"]
         return jsonify({"image_url": image_url})
 
     except Exception as e:
